b05f6289fdf7be090fa60e4a88294d4344183473
Nils Petzall <nils.petzall@gmail.com>
2026-01-04 10:55:33 +0100

    fix(sarif): add advisory locations
    * result per location (1 issue per place to fix, github)
    * refactored handling, so each diag.code can have it's processor
    * added compact dependency graph to markdown for all
    
    Since we required krates, split the call to print_diagnostics earlier
    so that sarif can have it's own arguments and make krates required.

--- src/sarif.rs
+++ src/sarif.rs
@@ -1,6 +1,8 @@
 mod collector;
 pub mod locator;
 pub mod model;
+mod processors;
 
 pub use collector::SarifCollector;
 pub use locator::{LocationFinder, Locator};
+pub use processors::{ProcessorSet};
